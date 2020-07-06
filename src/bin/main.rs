ec2998ea53c14fdf273d95822cd26141dbf805ee
ikanago <28985004+ikanago@users.noreply.github.com>
2020-07-06 22:17:36 +0900

    🛠 Fix wrong algorithm to build trie tree

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,13 @@ use trie_tree::trie_tree::TrieTree;
 
 fn main() {
     let mut tree = TrieTree::new();
-    tree.insert("hoge");
-    tree.insert("hot");
-    dbg!(tree);
+    tree.insert("A");
+    tree.insert("to");
+    tree.insert("tea");
+    tree.insert("ted");
+    tree.insert("ten");
+    tree.insert("i");
+    tree.insert("in");
+    tree.insert("inn");
+    dbg!(&tree);
 }
--- src/trie_tree.rs
+++ src/trie_tree.rs
@@ -1,7 +1,7 @@
 /// Node of trie tree.
 #[derive(Clone, Debug, Default)]
 pub struct TrieTree {
-    pub path: Option<String>,
+    pub path: String,
     children: Vec<Box<TrieTree>>,
 }
 
@@ -12,7 +12,7 @@ impl TrieTree {
 
     fn new_child(path: &str) -> Self {
         Self {
-            path: Some(path.to_string()),
+            path: path.to_string(),
             children: Vec::new(),
         }
     }
@@ -20,47 +20,59 @@ impl TrieTree {
     /// Return how many common character path of `TrieTree` nodes and an arugument have.
     fn longest_common_prefix(&self, other: &str) -> usize {
         let mut pos = 0;
-        if let Some(path_self) = &self.path {
-            for (char_self, char_other) in path_self.chars().zip(other.chars()) {
-                if char_self == char_other {
-                    pos += 1;
-                } else {
-                    break;
-                }
+        for (char_self, char_other) in self.path.chars().zip(other.chars()) {
+            if char_self == char_other {
+                pos += 1;
+            } else {
+                break;
             }
         }
         pos
     }
 
     pub fn insert(&mut self, new_path: &str) {
-        let lcp = self.longest_common_prefix(new_path);
-
-        dbg!(lcp);
-        if lcp == 0 {
-            if self.children.len() == 0 {
-                self.children.push(Box::new(TrieTree::new_child(new_path)));
-            } else {
-                for child in &mut self.children {
-                    child.insert(new_path);
-                }
-            }
+        // For the first time to insert node to root.
+        if self.path.len() == 0 && self.children.len() == 0 {
+            self.children.push(Box::new(TrieTree::new_child(new_path)));
             return;
         }
+
+        let lcp = self.longest_common_prefix(new_path);
         // If length of longest common prefix is not 0, `self.path` cannot be `None`.
-        let path = self.path.clone().unwrap();
-        // if `new_path` is prefix of `self.path`, it is ignored.
+        let path = self.path.clone();
         if path.len() > lcp {
             let common_prefix = &path[..lcp];
             let path_remaining = &path[lcp..];
             let new_path_remaining = &new_path[lcp..];
 
             let mut new_child = self.clone();
-            new_child.path = Some(path_remaining.to_string());
-            self.path = Some(common_prefix.to_string());
-            self.children = Vec::new();
-            self.children.push(Box::new(new_child));
-            self.children
-                .push(Box::new(TrieTree::new_child(new_path_remaining)))
+            new_child.path = path_remaining.to_string();
+            self.path = common_prefix.to_string();
+            self.children = vec![
+                Box::new(new_child),
+                Box::new(TrieTree::new_child(new_path_remaining)),
+            ]
+        } else {
+            let new_path_remaining = &new_path[lcp..];
+            let mut is_inserted = false;
+            for child in &mut self.children {
+                match (*child).path.chars().next() {
+                    // Because more than 2 children node do not have same prefix,
+                    // just check first character of key for each child.
+                    Some(first_char)
+                        if first_char == new_path_remaining.chars().next().unwrap() =>
+                    {
+                        is_inserted = true;
+                        child.insert(new_path_remaining);
+                        break;
+                    }
+                    _ => continue,
+                }
+            }
+            if !is_inserted {
+                self.children
+                    .push(Box::new(TrieTree::new_child(new_path_remaining)));
+            }
         }
     }
 }
@@ -72,9 +84,18 @@ mod tests {
     #[test]
     fn test_lcp() {
         let node_x = TrieTree {
-            path: Some("abcde".to_string()),
+            path: "abcde".to_string(),
             children: Vec::new(),
         };
         assert_eq!(node_x.longest_common_prefix("abchoge"), 3);
+    }
+
+    #[test]
+    fn test_lcp_root() {
+        let node_x = TrieTree {
+            path: "".to_string(),
+            children: Vec::new(),
+        };
+        assert_eq!(node_x.longest_common_prefix("abchoge"), 0);
     }
 }
